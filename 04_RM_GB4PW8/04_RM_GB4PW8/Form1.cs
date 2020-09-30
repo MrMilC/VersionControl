2b384dd45c4fc1f12a7526ffbf8d9848912b562a
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-09-30 15:38:32 +0200

    Load Data

--- 04_RM_GB4PW8/04_RM_GB4PW8/Form1.cs
+++ 04_RM_GB4PW8/04_RM_GB4PW8/Form1.cs
@@ -12,9 +12,17 @@ namespace _04_RM_GB4PW8
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
