2c9dfffb1c1494b9f47dd7a28c94e6c23fe7f481
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-10-14 15:43:32 +0200

    WebService

--- 06_RM_GB4PW8/06_RM_GB4PW8/Form1.cs
+++ 06_RM_GB4PW8/06_RM_GB4PW8/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _06_RM_GB4PW8.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,33 @@ namespace _06_RM_GB4PW8
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+
+            WebService();
+        }
+
+        private void WebService()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(i+"-"+result);
+            }
         }
     }
 }
