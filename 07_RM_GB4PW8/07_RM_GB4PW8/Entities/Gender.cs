085f4fa1546fbf018e36b60f72002e2772389a48
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-10-21 17:57:00 +0200

    Classes created

---
+++ 07_RM_GB4PW8/07_RM_GB4PW8/Entities/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_RM_GB4PW8.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}
