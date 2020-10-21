4f56df2c9c4cd985b5be4ece84c9c94f50465fac
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-10-21 20:07:23 +0200

    Simulation 2 Alpha

--- 07_RM_GB4PW8/07_RM_GB4PW8/Form1.cs
+++ 07_RM_GB4PW8/07_RM_GB4PW8/Form1.cs
@@ -31,7 +31,7 @@ namespace _07_RM_GB4PW8
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    //
+                    SimStep(year, i);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -106,6 +106,36 @@ namespace _07_RM_GB4PW8
             }
 
             return deathprobabilities;
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.DProbability).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.BProbability).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
     }
 }
