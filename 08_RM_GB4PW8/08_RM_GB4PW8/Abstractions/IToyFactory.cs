c6549a9e3dd990ef2fdad5f13f25467f94ce1c74
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-11-11 16:10:36 +0100

    IToyFactory created

---
+++ 08_RM_GB4PW8/08_RM_GB4PW8/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_RM_GB4PW8.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
