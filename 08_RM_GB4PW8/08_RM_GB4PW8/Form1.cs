c40e2d4df517d1db8100990971f59500ad5e2db4
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-11-11 17:52:54 +0100

    BallColor

--- 08_RM_GB4PW8/08_RM_GB4PW8/Form1.cs
+++ 08_RM_GB4PW8/08_RM_GB4PW8/Form1.cs
@@ -69,7 +69,10 @@ namespace _08_RM_GB4PW8
 
         private void btnSelectBall_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory()
+            {
+                BallColor = btnColor.BackColor
+            };
         }
 
         private void DisplayNext()
