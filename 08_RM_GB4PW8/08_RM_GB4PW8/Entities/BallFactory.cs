3cc696b8a9ee9b5b137777e33247021bda742db0
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-11-11 16:14:47 +0100

    BallFactory : IToyFactory

--- 08_RM_GB4PW8/08_RM_GB4PW8/Entities/BallFactory.cs
+++ 08_RM_GB4PW8/08_RM_GB4PW8/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _08_RM_GB4PW8.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace _08_RM_GB4PW8.Entities
 {
-    public class BallFactory
+    public class BallFactory: IToyFactory
     {
-        public Ball CreateNew()
+        public Toy CreateNew()
         {
             return new Ball();
         }
