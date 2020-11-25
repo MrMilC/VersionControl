5d6c984191396586fbfb5e379fd31870602cc12d
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-11-25 16:21:35 +0100

    Playtesting

--- 10_RM_GB4PW8/10_RM_GB4PW8/Form1.cs
+++ 10_RM_GB4PW8/10_RM_GB4PW8/Form1.cs
@@ -21,6 +21,9 @@ namespace _10_RM_GB4PW8
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
